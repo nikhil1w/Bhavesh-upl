855178c1bc2b046d6ed8d40ef39ec9c543fdd57c
nikhil1w <rayrambadan70@gmail.com>
2025-08-04 14:26:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,15 @@ class Config(object):
     DB_NAME = os.environ.get("DB_NAME")
     
 ####Static Configs Variables(Changeable)####
-    API_ID = 24660083 #API ID
-    API_HASH = "4532ca1450d9e1bcc15267b964023ef2" #API HASH
-    ADMIN_ID = 6801378994 #Admin ID
-    DB_URL = "mongodb+srv://master:vwdBu3pYSaROoYcR@cluster0.zihv6vx.mongodb.net/" #Database URL
-    TXT_LOG = -1002338959534 #Text Log Channel ID
-    LOG_CHANNEL = -100233895944 #Log Channel ID
-    CREDIT = "✞𝐑𝐚𝐎𝐧𝐞✞" #Credit Ko apne name se change kare
+    API_ID = 20535745 #API ID
+    API_HASH = "d917a3ee53e8334d110e0c71e534b95d" #API HASH
+    ADMIN_ID = 7780806801 #Admin ID
+    DB_URL = "mongodb+srv://editingtution99:kLKimOFEX1MN1v0G@cluster0.fxbujjd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #Database URL
+    TXT_LOG = -1002733606326 #Text Log Channel ID
+    LOG_CHANNEL = -1002733606326 #Log Channel ID
+    CREDIT = "Bhavesh" #Credit Ko apne name se change kare
     PROXY = "103.172.85.194:50100" #Socks5 Proxy
     KEY = b'm88=p?h,u4*I>d.|XaiMg~.?\:2{Yr+~' #Key 
     IV = b'*}~&;&$;*:-![@&>' #IV
+
     TASK_LIMIT = 5 #Multiple Task Limit at 1 time
